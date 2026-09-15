b3d438028ccc57288d277a8f8fefb646d24e7cfc
Minka Nagy <minkanagy@berkeley.edu>
2026-09-14 19:28:26 -0700

    ahhh

--- api/venue.tsx
+++ api/venue.tsx
@@ -52,7 +52,7 @@ export default async function handler(req: Request) {
   const appLink = `destin://location/${id ?? ''}`;
 
   const html = `<!DOCTYPE html>
-<html lang="en">
+<html lang="en">$
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
